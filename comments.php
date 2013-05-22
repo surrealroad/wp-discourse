25970ad951d18c45d2a540af2422f0cc5e297c79
surrealroad <sales@surrealroad.com>
2013-05-22 11:43:56 +0200

    Changes for Control Command Escape template
    Changes comments template for use with http://www.controlcommandescape.com

--- comments.php
+++ comments.php
@@ -26,29 +26,42 @@
 
 <?php # var_dump($discourse_info->posts) ?>
 
-<div id="comments">
+<div class="comments span10">
 <?php if(count($discourse_info->posts) > 0) { ?>
-<h2 id="comments-title">Notable Replies</h2>
-<?php } ?>
-		<ol class="commentlist">
-      <?php foreach($discourse_info->posts as &$post) { ?>
-        <li class="comment">
-				<div class="comment-author vcard">
-        <img alt="" src="<?php avatar($post->avatar_template,68) ?>" class="avatar avatar-68 photo avatar-default" height="68" width="68">
-          <a href="<?php homepage($options['url'],$post) ?>" rel="external" class="url"><?php echo ($show_fullname ? $post->name : $post->username) ?></a>
-          <br/>
-          <time pubdate="" datetime="<?php echo $post->created_at ?>"><?php echo mysql2date(get_option('date_format'), $post->created_at)?></time>
+<div class="comments-heading">
+<header class="section-header shadowed">
+<h4 id="comments-title"><i class="icon-comments-alt"></i> Notable Replies</h4>
+</header>
 </div>
-            <div class="comment-content"><?php echo $post->cooked ?></div>
-        </li>
+<?php } ?>
+		<ul class="commentlist">
+      <?php foreach($discourse_info->posts as &$post) { ?>
+<section class="comment media shadowed">
+<div class="pull-left media-object"><img alt="" src="<?php avatar($post->avatar_template,68) ?>" class="avatar avatar-68 photo avatar-default" height="68" width="68"></div>
+<div class="media-body">
+<header class="media-heading"><h4>
+<a href="<?php homepage($options['url'],$post); ?>" rel="external" class="url"><?php echo $post->username; ?></a>
+</h4></header>
+<div class="content"><?php echo $post->cooked; //var_dump($post);?></div>
+<div class="comment-meta commentmetadata">
+<i class="icon-time"></i> <time datetime="<?php echo mysql2date("Y-m-d", $post->created_at); ?>"><abbr class="timeago comment-published" title="<?php echo mysql2date("c", $post->created_at); ?>"><?php echo mysql2date(get_option('date_format'), $post->created_at); ?></abbr></time>
+</div>
+</div>
+<footer class="comment-gutter">
+		  <a class="btn" href="<?php echo $permalink."/".$post->post_number; ?>"><i class="icon-reply"></i> Reply</a>
+          <a class="btn" href="<?php echo $permalink."/".$post->post_number; ?>"><i class="icon-link"></i> Link</a>
+</footer>
+</section>
       <?php } ?>
+		</ul>
 
-		</ol>
-
-
-
-    <div id="respond">
-        <h3 id="reply-title"><a href="<?php echo $permalink ?>">Continue the discussion</a> at <?php echo $discourse_url_name ?></h3>
+		
+	
+    <div id="respond" class="comments-heading">
+<header class="section-header shadowed">
+        <h4 id="reply-title"><a href="<?php echo $permalink ?>"><i class="icon-arrow-right"></i> Continue the discussion at Control Command Escape Forums</a></h4>
+</header>
+</div>
         <?php if(count($discourse_info->posts) > 0) { ?>
         <p class='more-replies'><?php echo $more_replies ?></p>
         <p>
@@ -56,7 +69,5 @@
             <img alt="" src="<?php avatar($participant->avatar_template,25) ?>" class="avatar avatar-25 photo avatar-default" height="25" width="25">
           <?php } ?>
         </p>
-        <?php } ?>
-    </div><!-- #respond -->
-
+        <?php } ?>						
 </div>
